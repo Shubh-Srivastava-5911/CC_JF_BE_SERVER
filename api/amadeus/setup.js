8b649fa0985665d0fb157c9fbc84e30f857b6748
Shubh <shubhs.2803@gmail.com>
2024-10-13 03:56:05 +0530

    seventh update

--- api/amadeus/setup.js
+++ api/amadeus/setup.js
@@ -1,32 +1,3 @@
-// const axios = require('axios');
-// const qs = require('qs'); // Used to stringify the request body
-// const str = require('./string.json');
-
-// const client_id = str.apiKey;
-// const client_secret = str.apiSecret;
-
-// // Define the data as x-www-form-urlencoded
-// const data = qs.stringify({
-//     grant_type: 'client_credentials',
-//     client_id: client_id,
-//     client_secret: client_secret
-// });
-
-// function use() {
-//     // Send POST request to Amadeus API
-//     axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', data, {
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         }
-//     })
-//     .then(response => {
-//         console.log(response.data);  // The access token will be here
-//     })
-//     .catch(error => {
-//         console.error('Error:', error.response ? error.response.data : error.message);
-//     });
-// }
-
 const str = require('./string.json');
 
 const Amadeus = require('amadeus');
@@ -89,3 +60,35 @@ module.exports = class FlightEndpoint {
         // return the list
     }
 }
+
+
+
+// const axios = require('axios');
+// const qs = require('qs'); // Used to stringify the request body
+// const str = require('./string.json');
+
+// const client_id = str.apiKey;
+// const client_secret = str.apiSecret;
+
+// // Define the data as x-www-form-urlencoded
+// const data = qs.stringify({
+//     grant_type: 'client_credentials',
+//     client_id: client_id,
+//     client_secret: client_secret
+// });
+
+// function use() {
+//     // Send POST request to Amadeus API
+//     axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', data, {
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         }
+//     })
+//     .then(response => {
+//         console.log(response.data);  // The access token will be here
+//     })
+//     .catch(error => {
+//         console.error('Error:', error.response ? error.response.data : error.message);
+//     });
+// }
+
