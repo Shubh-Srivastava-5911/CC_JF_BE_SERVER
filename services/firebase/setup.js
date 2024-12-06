61f30e451212e48e0ecb2a474b1aa6c6a0e5ec35
Shubh <shubhs.2803@gmail.com>
2024-12-07 01:11:39 +0530

    nb26 update

--- services/firebase/setup.js
+++ services/firebase/setup.js
@@ -1,36 +1,20 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCDE6Pq7bJ3AYAYYug9Bld5_9HgKutvclk",
-//   authDomain: "journeyflow-f7d8a.firebaseapp.com",
-//   projectId: "journeyflow-f7d8a",
-//   storageBucket: "journeyflow-f7d8a.firebasestorage.app",
-//   messagingSenderId: "768696247655",
-//   appId: "1:768696247655:web:30f674982a213b5e34bbc2",
-//   measurementId: "G-K6TFR579F7"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
-const serviceAccount = require("./servicesec.json");
+// const serviceAccount = require("./servicesec.json");
+
+// Load environment variables from .env for local development
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
 
 module.exports = class FirebaseEndpoint {
     static db;
 
     static {
         initializeApp({
-            credential: cert(serviceAccount)
+            credential: cert(JSON.parse(serviceAccount))
         });
         this.db = getFirestore();
     }
